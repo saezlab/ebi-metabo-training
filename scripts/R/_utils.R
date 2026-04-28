ec2fb8dac8944375173c17184dac7819f9935a64
Christina Schmidt <christina.schmidt1@outlook.de>
2026-04-28 15:15:49 +0200

    added helper functions for 02.

--- scripts/R/_utils.R
+++ scripts/R/_utils.R
@@ -50,3 +50,108 @@ mp_peek <- function(x, n_rows = 6L, n_cols = 8L) {
     }
     head(x, n_rows)
 }
+
+# Establish helper functions to create some overview summaries that we can use to
+# discuss the individual steps we will be doing within 02_id_workflow.R:
+
+# 1. Helper: extract one summary row from count_id() output
+extract_id_counts <- function(count_obj, row_name) {
+    ## count_obj must be an object returned by MetaProViz::count_id()
+
+    tbl <- count_obj$Table
+
+    ## Count how many features fall into each ID class
+    counts_wide <- tbl %>%
+        count(id_label, name = "n") %>%
+        mutate(
+            id_label = recode(
+                id_label,
+                "No ID" = "no_ID",
+                "Single ID" = "Single_ID",
+                "Multiple IDs" = "Multiple_IDs"
+            )
+        ) %>%
+        pivot_wider(
+            names_from = id_label,
+            values_from = n,
+            values_fill = 0
+        )
+
+    ## Make sure all expected columns exist
+    ## This avoids errors if one class is absent in a given dataset
+    for (col in c("no_ID", "Single_ID", "Multiple_IDs")) {
+        if (!col %in% names(counts_wide)) {
+            counts_wide[[col]] <- 0L
+        }
+    }
+
+    ## Build one clean summary row
+    tibble(
+        row_name = row_name,
+        no_ID = as.integer(counts_wide$no_ID[1]),
+        Single_ID = as.integer(counts_wide$Single_ID[1]),
+        Multiple_IDs = as.integer(counts_wide$Multiple_IDs[1]),
+        Total_IDs = as.integer(sum(tbl$entry_count, na.rm = TRUE))
+    )
+}
+
+
+
+# 2. Helper: append one summary row to id_count_df
+append_id_counts <- function(df, count_obj, row_name) {
+    bind_rows(df, extract_id_counts(count_obj, row_name))
+}
+
+# 3. Helper: calculate deltas within each database only
+calculate_id_deltas <- function(df, from_category, to_category) {
+
+    df %>%
+        separate(row_name, into = c("id_type", "category"), sep = "_", remove = FALSE) %>%
+        select(-row_name) %>%
+        pivot_wider(
+            names_from = category,
+            values_from = c(no_ID, Single_ID, Multiple_IDs, Total_IDs)
+        ) %>%
+        transmute(
+            row_name = paste0("delta_", id_type, "_", from_category, "_", id_type, "_", to_category),
+            no_ID = .data[[paste0("no_ID_", to_category)]] - .data[[paste0("no_ID_", from_category)]],
+            Single_ID = .data[[paste0("Single_ID_", to_category)]] - .data[[paste0("Single_ID_", from_category)]],
+            Multiple_IDs = .data[[paste0("Multiple_IDs_", to_category)]] - .data[[paste0("Multiple_IDs_", from_category)]],
+            Total_IDs = .data[[paste0("Total_IDs_", to_category)]] - .data[[paste0("Total_IDs_", from_category)]]
+        )
+}
+
+# 4. Helper: count features with no HMDB, KEGG, or PUBCHEM. This works on the
+#original category dataframe, not on count_id() output.
+count_none_all_ids <- function(df) {
+    sum(
+        (is.na(df$HMDB)    | trimws(as.character(df$HMDB)) %in% c("", "NA")) &
+            (is.na(df$KEGG)    | trimws(as.character(df$KEGG)) %in% c("", "NA")) &
+            (is.na(df$PUBCHEM) | trimws(as.character(df$PUBCHEM)) %in% c("", "NA")),
+        na.rm = TRUE
+    )
+}
+
+# 5. Helper: append one row to none_summary_df
+append_none_summary <- function(df, input_df, row_name) {
+    bind_rows(
+        df,
+        tibble(
+            row_name = row_name,
+            No_database_ID = as.integer(count_none_all_ids(input_df))
+        )
+    )
+}
+
+# 6. Helper: calculate delta for the "none at all" summary
+calculate_none_delta <- function(df, from_category, to_category) {
+
+    tibble(
+        row_name = paste0("delta_", from_category, "_", to_category),
+        No_database_ID =
+            df$No_database_ID[df$row_name == to_category] -
+            df$No_database_ID[df$row_name == from_category]
+    )
+}
+
+
