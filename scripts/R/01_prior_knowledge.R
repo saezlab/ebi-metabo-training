b43402b6b0c98d8fb94d9ae2326f070c8d4501c3
Christina Schmidt <christina.schmidt1@outlook.de>
2026-04-29 17:56:59 +0200

    Update 01_prior_knowledge.R

--- scripts/R/01_prior_knowledge.R
+++ scripts/R/01_prior_knowledge.R
@@ -36,12 +36,89 @@ library(MetaProViz)
 library(OmnipathR)
 
 # %% [markdown]
-
+# ## 1. MetaProViz MetSigDB
+# Here we will showcase some content of the Metabolite Signature database (MetSigDB),
+# which is a curated collection of metabolite sets for enrichment analysis.
+# It is built on top of OmnipathR and provides ready-to-use tables for pathway-style analyses.
 
 # %%
 
 
 # %% [markdown]
+# ### 1.1. Pathway-metabolite sets
+# MetaProViz includes KEGG, wikipathwyas, reactome
+
+# %%
+
+# KEGG
+KEGG_Pathways <- metsigdb_kegg(exclude_metabolites = "all")# default= all
+
+#Reactome
+metsigdb_reactome()
+
+
+# %% [markdown]
+# ### 1.2. Pathway-gene-metabolite sets
+# MetaProViz convert gene-sets to gene-metabolite sets and includes two genesets,
+# Hallmarks and Gaude
+
+# %%
+
+# Gaude
+data("gaude_pathways")
+
+gaude_pathways <- make_gene_metab_set( #MetaproViz function
+    input_pk =   gaude_pathways,
+    metadata_info = c(Target = "gene"),
+    pk_name = "gaude_pathways",
+    save_table = NULL,
+    exclude_metabolites = "all"
+)$GeneMetabSet %>%
+    mutate(
+        gene_metab_source = "gaude_pathways",
+        interaction_type = if_else(grepl("^HMDB", feature), "pathway-metabolite", "pathway-metabolic_enzyme")
+    )
+
+
+# Hallmark
+data("hallmarks")
+
+hallmarks_pathways <- make_gene_metab_set(
+    input_pk = hallmarks,
+    metadata_info = c(Target = "gene"),
+    pk_name = "hallmarks",
+    save_table = NULL,
+    exclude_metabolites = "all"
+)$GeneMetabSet %>%
+    mutate(
+        gene_metab_source = "hallmarks",
+        interaction_type = if_else(grepl("^HMDB", feature), "pathway-metabolite", "pathway-metabolic_enzyme")
+    )
+
+
+# %% [markdown]
+# ### 1.3. Chemical Class -metabolite sets
+# Based on RaMP-2.0 calssification using CallyFire
+
+# %%
+
+# Chemical class
+ChemicalClass <- metsigdb_chemicalclass()
+
+
+# %% [markdown]
+# ### 1.4. Cancer-metabolite sets
+# Based on macDB content
+
+# %%
+
+# Cancer
+Cancer <- metsigdb_macdb()
+
+
+
+# %% [markdown]
+# ### 1.5. Metabolite-receptor, -transporter, -enzyme sets from MetalinksDB
 # MetaProViz wraps MetalinksDB into a ready-to-enrich pathway-style table
 # via `metsigdb_metalinks()`:
 
@@ -49,8 +126,26 @@ library(OmnipathR)
 ml_pk <- metsigdb_metalinks()
 head(ml_pk)
 
+ml_pk_expansion <- tibble::as_tibble(ml_pk)
+unknown <- unique(ml_pk_expansion[["interaction_family"]][!ml_pk_expansion[["interaction_family"]] %in% c(
+    "Other protein-metabolite",
+    "Transporter-metabolite",
+    "Receptor-metabolite",
+    "Enzyme-metabolite"
+)])
+unknown <- unknown[!is.na(unknown)]
 
-
+ml_pk_expansion <- list(
+    metsigdb_metalinks = ml_pk_expansion,
+    metsigdb_metalinks__other_protein_metabolite =
+        dplyr::filter(ml_pk_expansion, interaction_family == "Other protein-metabolite"),
+    metsigdb_metalinks__transporter_metabolite =
+        dplyr::filter(ml_pk_expansion, interaction_family == "Transporter-metabolite"),
+    metsigdb_metalinks__receptor_metabolite =
+        dplyr::filter(ml_pk_expansion, interaction_family == "Receptor-metabolite"),
+    metsigdb_metalinks__enzyme_metabolite =
+        dplyr::filter(ml_pk_expansion, interaction_family == "Enzyme-metabolite")
+)
 
 # %% [markdown]
 # ## Compare PK coverage
@@ -59,21 +154,10 @@ head(ml_pk)
 # `compare_pk()` to see the intersection / specificity.
 
 # %%
-pk_overlap <- compare_pk(
-    data = list(
-        metalinks = as.data.frame(ml_pk),
-        kegg      = as.data.frame(metsigdb_kegg())
-    ),
-    metadata_info = list(
-        metalinks = c("metabolite"),
-        kegg      = c("MetaboliteID")
-    ),
-    plot_name = "Metabolite coverage: MetalinksDB vs KEGG",
-    save_plot = NULL,
-    print_plot = FALSE,
-    path = mp_results_dir("04_prior_knowledge")
+pk_comp_res <- compare_pk(data = list(Hallmarks = as.data.frame(hallmarks_pathways),
+                                      Gaude = as.data.frame(gaude_pathways),
+                                      MetalinksDB = as.data.frame(ml_pk))
 )
-
 
 
 
